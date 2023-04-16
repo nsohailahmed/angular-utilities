0854925ab4c774f42573a6ae99ae515499fcdcf4
nsohailahmed <nsohailahmed@gmail.com>
2023-04-17 00:11:18 +0530

    angular inbuil pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-utilities';
+
+  name:string = '';
+  date:string = '';
+  amount:number = 0;
+  height:number = 0;
+  onNameChange(nameEvent:any){
+    this.name = nameEvent.target.value;
+  }
+  onDateChange(dateEvent:any){
+    this.date = dateEvent.target.value;
+  }
+  onAmountChange(amountEvent:any){
+    this.amount = amountEvent.target.value;
+  }
+  onHeightChange(heightEvent:any){
+    this.height = heightEvent.target.value;
+  }
 }
